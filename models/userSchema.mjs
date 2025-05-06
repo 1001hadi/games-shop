205793fb85582d5a936b9bf702043a0a4bc83904
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-06 15:12:13 -0600

    Schema added user,cart and game

---
+++ models/userSchema.mjs
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+// building schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide your first name"],
+    trim: true,
+    minLength: [3, "First name can't be less than 3 characters long"],
+    maxLength: [20, "First name can't be more than 20 characters long"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide your last name"],
+    trim: true,
+    minLength: [3, "Last name can't be less than 3 characters long"],
+    maxLength: [20, "Last name can't be more than 20 characters long"],
+  },
+  age: {
+    type: Number,
+    min: [13, "Must be over 13 to purchases!"],
+    max: [55, "Come on, Go work, you better save for your retirement!"],
+    validate: {
+      validator: Number.isInteger,
+      message: "please enter your age!",
+    },
+    index: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email address"],
+    trim: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email address must be valid !",
+    ],
+    // to create index with mongoose
+    index: true,
+  },
+  pwd: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 123,
+//     pattern:
+//       "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,128}$",
+//     examples: ["P@$$wOrd123", "SecurePassword!", "MyPa$$wOrd"],
+//   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+    // properties: {
+    //   username: {
+    //     type: String,
+    //     minLength: 3,
+    //     maxLength: 30,
+    //     description: "Administrator username.",
+    //   },
+    //   password: {
+    //     type: String,
+    //     minLength: 8,
+    //     maxLength: 30,
+    //     description: "Administrator password.",
+    //   },
+    // },
+  },
+  cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart" },
+  registrationDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("User", userSchema);
